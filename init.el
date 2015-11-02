50637590b0ab81a7c7604c1c45d498b6ac2758ee
John Jacobsen <jjacobsen@opinionlab.com>
2015-11-02 10:42:59 -0600

    Do away w/ pesky graphical dialog boxes.

--- init.el
+++ init.el
@@ -499,4 +499,19 @@
 
 (setq vc-make-backup-files t)
 
+
+;; Pesky dialog boxes :-(
+;; http://superuser.com/questions/125569/how-to-fix-emacs-popup-dialogs-on-mac-os-x
+(defadvice yes-or-no-p (around prevent-dialog activate)
+  "Prevent yes-or-no-p from activating a dialog"
+  (let ((use-dialog-box nil))
+    ad-do-it))
+
+
+(defadvice y-or-n-p (around prevent-dialog-yorn activate)
+  "Prevent y-or-n-p from activating a dialog"
+  (let ((use-dialog-box nil))
+    ad-do-it))
+
+
 (provide 'init)
