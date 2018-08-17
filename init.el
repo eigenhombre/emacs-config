641a74bc862acdfb105adac15b550fd835d8e58d
John Jacobsen <jjacobsen@opploans.com>
2018-08-17 11:17:07 -0500

    Fix reverse-transpose-sexps

--- init.el
+++ init.el
@@ -706,7 +706,7 @@
   ;; when transpose-sexps can no longer transpose, it throws an error and code
   ;; below this line won't be executed. So, we don't have to worry about side
   ;; effects of backward-sexp and forward-sexp.
-  (backward-sexp (1+ arg))
+  (backward-sexp arg)
   (forward-sexp 1))
 
 
